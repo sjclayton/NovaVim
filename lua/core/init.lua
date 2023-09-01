5de0e4e33345f51ede3c9c8d6b3543552a638627
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-09-01 11:15:47 -0400

    chore(core): prep for first plugin setups

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -1,8 +1,20 @@
--- Ensure leader key is mapped before anything else happens.
-vim.g.mapleader = ' '
-
-require('core.lazy')
 require('core.options')
-require('core.keymap')
+require('core.lazy')
+
+if vim.fn.argc(-1) == 0 then
+  -- autocmds and keymaps can wait to load
+  vim.api.nvim_create_autocmd('User', {
+    group = vim.api.nvim_create_augroup('NovaVim', { clear = true }),
+    pattern = 'VeryLazy',
+    callback = function()
+      require('core.autocmds')
+      require('core.keymaps')
+    end,
+  })
+else
+  -- load them now so they affect the opened buffers
+  require('core.autocmds')
+  require('core.keymaps')
+end
 
 vim.cmd.colorscheme('catppuccin')
