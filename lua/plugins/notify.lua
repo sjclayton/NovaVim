a73d2ba6a337e7efd73c6fb57b9ce2666892cc7e
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-09-06 14:48:34 -0400

    Revert "feat(core.util): add util functions in separate file"
    This reverts commit ab99dc9a1a2cafe35f4dfff48eac5606120145d6.

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,6 +1,7 @@
 return {
   {
     'rcarriga/nvim-notify',
+    -- event = 'VeryLazy',
     keys = {
       {
         '<leader>un',
@@ -19,14 +20,5 @@ return {
         return math.floor(vim.o.columns * 0.75)
       end,
     },
-    init = function()
-      -- If noice is not enabled, setup notify on VeryLazy
-      local Util = require('core.util')
-      if not Util.has('noice.nvim') then
-        Util.on_very_lazy(function()
-          vim.notify = require('notify')
-        end)
-      end
-    end,
   },
 }
