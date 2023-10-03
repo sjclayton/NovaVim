81afd40f57b98c8ee50446a1e3911e8a0d59204c
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-03 10:20:01 -0400

    feat(plugins.ui.fidget): add initial setup of fidget.nvim

---
+++ lua/plugins/ui/fidget.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    'j-hui/fidget.nvim',
+    tag = 'legacy',
+    event = 'LspAttach',
+    opts = {
+      window = {
+        relative = 'win', -- where to anchor, either "win" or "editor"
+        blend = 0, -- &winblend for the window
+        zindex = nil, -- the zindex value for the window
+        border = 'none', -- style of border for the fidget window
+      },
+      fmt = {
+        stack_upwards = false,
+      },
+    },
+  },
+}
