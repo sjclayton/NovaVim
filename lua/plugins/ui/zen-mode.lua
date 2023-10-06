09cc247ae443c8594d8f3fdc5796cf080bdcbec1
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-06 19:44:24 -0400

    feat(plugins.ui.zen-mode): hide gitsigns and statusline in zen-mode

--- lua/plugins/ui/zen-mode.lua
+++ lua/plugins/ui/zen-mode.lua
@@ -13,6 +13,12 @@ return {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      plugins = {
+        options = {
+          laststatus = 0,
+        },
+        gitsigns = { enabled = true },
+      },
     },
   },
 }
