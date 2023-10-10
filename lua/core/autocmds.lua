3bbb76fae5145c70cd3df8dfc01ccdf4a56cf0ed
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-10 06:24:26 -0400

    feat(core.autocmds): add autocmd for alpha on last buffer, add fix for telescope insert bug

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -12,7 +12,7 @@ vim.api.nvim_create_autocmd('BufReadPost', {
     if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
       return
     end
-    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local mark = vim.api.nvim_buf_get_mark(buf, '.')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
@@ -30,22 +30,32 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
--- When there is no buffer left show Alpha dashboard
+-- When there is no buffer left show the Alpha dashboard
 -- requires "famiu/bufdelete.nvim" and "goolord/alpha-nvim"
 
-local alpha_on_empty = vim.api.nvim_create_augroup('alpha_on_empty', { clear = true })
 vim.api.nvim_create_autocmd('User', {
   pattern = 'BDeletePost*',
-  group = alpha_on_empty,
+  group = augroup('alpha_on_empty'),
   callback = function(event)
     local fallback_name = vim.api.nvim_buf_get_name(event.buf)
-    local fallback_ft = vim.api.nvim_buf_get_option(event.buf, 'filetype')
+    local fallback_ft = vim.api.nvim_get_option_value('filetype', { buf = event.buf })
     local fallback_on_empty = fallback_name == '' and fallback_ft == ''
 
     if fallback_on_empty then
       -- require("neo-tree").close_all()
       vim.api.nvim_command('Alpha')
       vim.api.nvim_command(event.buf .. 'bwipeout')
+    end
+  end,
+})
+
+-- HACK: Workaround to fix annoying buggy Telescope behaviour of being put into insert mode after closing.
+
+vim.api.nvim_create_autocmd('WinLeave', {
+  group = augroup('telescope_leave'),
+  callback = function()
+    if vim.bo.ft == 'TelescopePrompt' and vim.fn.mode() == 'i' then
+      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<Esc>', true, false, true), 'i', false)
     end
   end,
 })
