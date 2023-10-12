2f0645cd4d0bf6b32bf422a35ad795d1fc4e6c85
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-11 20:58:23 -0400

    feat(core.autocmds): add autocmd for highlight on yank, fix last_loc autocmd issue

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,17 +2,27 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('nova_' .. name, { clear = true })
 end
 
+-- Highlight on yank
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = augroup('highlight_yank'),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- Go to last edit location when opening a buffer.
 
 vim.api.nvim_create_autocmd('BufReadPost', {
   group = augroup('last_loc'),
-  callback = function()
+  callback = function(event)
     local exclude = { 'gitcommit' }
-    local buf = vim.api.nvim_get_current_buf()
-    if vim.tbl_contains(exclude, vim.bo[buf].filetype) then
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].nova_last_loc then
       return
     end
-    local mark = vim.api.nvim_buf_get_mark(buf, '.')
+    vim.b[buf].nova_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
     local lcount = vim.api.nvim_buf_line_count(buf)
     if mark[1] > 0 and mark[1] <= lcount then
       pcall(vim.api.nvim_win_set_cursor, 0, mark)
