5de532200cadc812f6bf7113916e5e06f213c76e
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-12 20:13:03 -0400

    fix(plugins.ui.alpha): fix bug with Lazy killing keymaps on plugin install

--- lua/plugins/ui/alpha.lua
+++ lua/plugins/ui/alpha.lua
@@ -68,31 +68,37 @@ return {
 
     local function buttons()
       local keybind_opts = { silent = true, noremap = true }
-      vim.api.nvim_create_autocmd({ 'User' }, {
-        pattern = { 'AlphaReady' },
-        callback = function(_)
-          -- General
-          vim.api.nvim_buf_set_keymap(0, 'n', 'n', ':ene <BAR> startinsert <CR>', keybind_opts)
+      -- General
+      vim.api.nvim_buf_set_keymap(0, 'n', 'n', ':ene <BAR> startinsert <CR>', keybind_opts)
 
-          -- Harpoon
-          vim.api.nvim_buf_set_keymap(0, 'n', 'h', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', keybind_opts)
+      -- Harpoon
+      vim.api.nvim_buf_set_keymap(0, 'n', 'h', ':lua require("harpoon.ui").toggle_quick_menu()<cr>', keybind_opts)
 
-          -- Telescope
-          vim.api.nvim_buf_set_keymap(0, 'n', 'j', ':Telescope oldfiles<CR>', keybind_opts)
-          vim.api.nvim_buf_set_keymap(0, 'n', 'k', ':lua require"core.helpers".project_files() <CR>', keybind_opts)
+      -- Neo-tree
+      vim.api.nvim_buf_set_keymap(
+        0,
+        'n',
+        'e',
+        ':lua require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })<cr>',
+        keybind_opts
+      )
 
-          -- Lazy
-          vim.api.nvim_buf_set_keymap(0, 'n', 'l', ':Lazy update<CR>', keybind_opts)
+      -- Telescope
+      vim.api.nvim_buf_set_keymap(0, 'n', 'j', ':Telescope oldfiles<CR>', keybind_opts)
+      vim.api.nvim_buf_set_keymap(0, 'n', 'k', ':lua require"core.helpers".project_files() <CR>', keybind_opts)
 
-          -- Configs
-          vim.api.nvim_buf_set_keymap(0, 'n', 'm', ':e ~/.config/NovaVim/lua/core/keymaps.lua<CR>', keybind_opts)
-          vim.api.nvim_buf_set_keymap(0, 'n', 'o', ':e ~/.config/NovaVim/lua/core/options.lua<CR>', keybind_opts)
+      -- Lazy
+      vim.api.nvim_buf_set_keymap(0, 'n', 'l', ':Lazy update<CR>', keybind_opts)
 
-          -- Quit
-          vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<cmd>q<CR>', keybind_opts)
-        end,
-      })
+      -- Configs
+      vim.api.nvim_buf_set_keymap(0, 'n', 'm', ':e ~/.config/NovaVim/lua/core/keymaps.lua<CR>', keybind_opts)
+      vim.api.nvim_buf_set_keymap(0, 'n', 'o', ':e ~/.config/NovaVim/lua/core/options.lua<CR>', keybind_opts)
 
+      -- Quit
+      vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<cmd>q<CR>', keybind_opts)
+
+      -- NOTE: This is a mess, if you add or remove shortcuts the table below will have to be played around with
+      -- to get the color highlights to look right.
       local buttons_hl = {
         { 'AlphaButtons', 0, 20 },
         { 'AlphaShortcut', 21, 23 },
@@ -100,8 +106,8 @@ return {
         { 'AlphaButtons', 30, 49 },
         { 'AlphaShortcut', 51, 55 },
         { 'Comment', 56, 62 },
-        { 'AlphaButtons', 63, 85 },
-        { 'AlphaShortcut', 86, 89 },
+        { 'AlphaButtons', 63, 83 },
+        { 'AlphaShortcut', 84, 89 },
       }
 
       local buttons_ln = {
@@ -113,10 +119,10 @@ return {
           type = 'text',
           val = {
             '                        │                           │                       ',
-            '   New File       n    │       terminal      t    │    󰒲   Lazy Update   l',
-            '󱨻   Recent Files   j    │       lf            ;    │                       ',
-            '   Git Files      k    │       search        s    │    󰌓   Edit Keymaps  m',
-            '󱡅   Harpoon        h    │    פּ   nvim-tree          │       Edit Options  o',
+            '   New File       n    │    󰙅   Explorer      e    │    󰒲   Lazy Update   l',
+            '󱨻   Recent Files   j    │                           │                       ',
+            '   Git Files      k    │                           │    󰌓   Edit Keymaps  m',
+            '󱡅   Harpoon        h    │                           │       Edit Options  o',
             '                        │                           │                       ',
             '                        │    󰗼   Quit          q    │                       ',
             '                        │                           │                       ',
@@ -141,10 +147,8 @@ return {
     local button_group = { type = 'group', val = buttons }
 
     local function footer_text()
-      ---@diagnostic disable-next-line:undefined-field
       local lazy_stats = require('lazy').stats()
       local ms = (math.floor(lazy_stats.startuptime * 100 + 0.5) / 100)
-      -- stylua: ignore
       local total_plugins = '⚡ Lazy loaded ' .. lazy_stats.count .. ' plugins in ' .. ms .. ' ms'
       vim.api.nvim_create_autocmd({ 'User' }, {
         pattern = { 'LazyVimStarted' },
@@ -166,18 +170,6 @@ return {
       },
     }
 
-    -- close Lazy and re-open when the dashboard is ready
-    if vim.o.filetype == 'lazy' then
-      vim.cmd.close()
-      vim.api.nvim_create_autocmd('User', {
-        once = true,
-        pattern = 'AlphaReady',
-        callback = function()
-          require('lazy').show()
-        end,
-      })
-    end
-
     local marginTopPercent = 0.25
     local headerPadding = fn.max({ 2, fn.floor(fn.winheight(0) * marginTopPercent) })
 
@@ -195,5 +187,17 @@ return {
     }
 
     alpha.setup(config)
+
+    -- close Lazy and re-open when the dashboard is ready
+    if vim.o.filetype == 'lazy' then
+      vim.cmd.close()
+      vim.api.nvim_create_autocmd('User', {
+        once = true,
+        pattern = 'AlphaReady',
+        callback = function()
+          require('lazy').show()
+        end,
+      })
+    end
   end,
 }
