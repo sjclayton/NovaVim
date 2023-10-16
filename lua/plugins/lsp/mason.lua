43ada9f4cf3585fad7bb4f0021902d2174ebc844
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-16 15:42:46 -0400

    feat(plugins.lsp.mason): add auto install for non lsp tools

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -3,18 +3,50 @@ return {
     'williamboman/mason.nvim',
     cmd = 'Mason',
     keys = {
-      { '<leader>cm', '<CMD>Mason<cr>', desc = 'Open Mason', noremap = true },
+      { '<leader>cm', '<CMD>Mason<cr>', desc = 'Open Mason' },
     },
-    config = function()
-      -- import mason
-      local mason = require('mason')
+    build = ':MasonUpdate',
+    opts = {
+      ensure_installed = {
+        -- List of other tools to be installed automatically (DAP, linters, formatters)
+        -- NOTE: Language server auto installation handled by mason-lspconfig.
 
-      -- configure mason
-      mason.setup({
-        ui = {
-          border = 'rounded',
-        },
-      })
+        -- Golang
+        'gofumpt',
+        'goimports-reviser',
+
+        -- Lua
+        'stylua',
+      },
+      ui = {
+        border = 'rounded',
+      },
+    },
+    config = function(_, opts)
+      require('mason').setup(opts)
+      local mr = require('mason-registry')
+      mr:on('package:install:success', function()
+        vim.defer_fn(function()
+          -- trigger FileType event to possibly load newly installed LSP server
+          require('lazy.core.handler.event').trigger({
+            event = 'FileType',
+            buf = vim.api.nvim_get_current_buf(),
+          })
+        end, 100)
+      end)
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
     end,
   },
 }
