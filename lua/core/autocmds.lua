aac2275b1e74e3e948dc4748a50e651cb516c9cf
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-16 14:55:22 -0400

    feat(core.autocmds): add autocmd to create intermediate directories if non-existent when writing file

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -40,6 +40,19 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+
+vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+  group = augroup('auto_create_dir'),
+  callback = function(event)
+    if event.match:match('^%w%w+://') then
+      return
+    end
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
+  end,
+})
+
 -- When there is no buffer left show the Alpha dashboard
 -- requires "famiu/bufdelete.nvim" and "goolord/alpha-nvim"
 
