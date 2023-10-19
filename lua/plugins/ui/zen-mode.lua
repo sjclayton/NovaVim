0a3ac95ce0027ed884b4731467799a8437f423b9
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-10-19 06:48:29 -0400

    fix(plugins.ui.zen-mode): fix ts-context not being visible

--- lua/plugins/ui/zen-mode.lua
+++ lua/plugins/ui/zen-mode.lua
@@ -2,22 +2,23 @@ return {
   {
     'folke/zen-mode.nvim',
     keys = {
-      {
-        '<leader>uZ',
-        ':ZenMode<CR>',
-        desc = 'Toggle zen mode',
-        noremap = true,
-      },
+      { '<leader>uZ', '<cmd>ZenMode<cr>', desc = 'Toggle zen mode' },
     },
     opts = {
       -- your configuration comes here
       -- or leave it empty to use the default settings
       -- refer to the configuration section below
+      zindex = 20,
+      window = {
+        backdrop = 1,
+        width = 0.75,
+      },
       plugins = {
         options = {
           laststatus = 0,
         },
         gitsigns = { enabled = true },
+        tmux = { enabled = true },
       },
     },
   },
