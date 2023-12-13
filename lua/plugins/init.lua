75491acb4eb8029593813e0dbc534b80aaa898c2
Shaun Clayton <shaun.j.clayton@gmail.com>
2023-12-13 13:47:50 -0500

    feat(core.plugins): start restructuring plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,23 @@
+-- return {
+--
+--   --- Base plugins
+--
+--   { import = 'plugins.ai' },
+--   { import = 'plugins.coding' },
+--   { import = 'plugins.colorschemes' },
+--   { import = 'plugins.editor' },
+--   { import = 'plugins.lsp' },
+--   { import = 'plugins.ui' },
+--   { import = 'plugins.utils' },
+--
+-- }
+--
+
 return {
-
-  --- Base plugins
-
-  { import = 'plugins.ai' },
-  { import = 'plugins.coding' },
-  { import = 'plugins.colorschemes' },
-  { import = 'plugins.editor' },
-  { import = 'plugins.lsp' },
-  { import = 'plugins.ui' },
-  { import = 'plugins.utils' },
-
-  --- Language configs and plugins
-
-  -- { import = 'plugins.lsp.langs' },
+  -- General Plugins
+  -- Language Specific
+  -- LSP
+  -- Treesitter
+  -- UI
+  -- Utils
 }
