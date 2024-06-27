2ab4eae21d8b581629f88e9400fd41e6833dc85f
Shaun Clayton <shaun.j.clayton@gmail.com>
2024-06-27 12:01:20 -0400

    feat(core.autocmds): add more helpful autocmds

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,12 +2,34 @@ local function augroup(name)
   return vim.api.nvim_create_augroup('nova_' .. name, { clear = true })
 end
 
+-- Check if we need to reload the file when it changed
+
+vim.api.nvim_create_autocmd({ 'FocusGained', 'TermClose', 'TermLeave' }, {
+  group = augroup('checktime'),
+  callback = function()
+    if vim.o.buftype ~= 'nofile' then
+      vim.cmd('checktime')
+    end
+  end,
+})
+
 -- Highlight on yank
 
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = augroup('highlight_yank'),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Resize splits if window got resized
+
+vim.api.nvim_create_autocmd({ 'VimResized' }, {
+  group = augroup('resize_splits'),
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd('tabdo wincmd =')
+    vim.cmd('tabnext ' .. current_tab)
   end,
 })
 
@@ -33,8 +55,8 @@ vim.api.nvim_create_autocmd('BufReadPost', {
 -- Set line wrap in text filetypes
 
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup('wrap_spell'),
-  pattern = { 'gitcommit', 'markdown', 'text' },
+  group = augroup('wrap'),
+  pattern = { 'gitcommit', 'markdown', '*.txt' },
   callback = function()
     vim.opt_local.wrap = true
   end,
@@ -69,6 +91,7 @@ vim.api.nvim_create_autocmd('FileType', {
   pattern = {
     'PlenaryTestPopup',
     'checkhealth',
+    'gitsigns.blame',
     'help',
     'lspinfo',
     'man',
@@ -77,7 +100,6 @@ vim.api.nvim_create_autocmd('FileType', {
     'neotest-summary',
     'notify',
     'qf',
-    'query',
     'startuptime',
     'tsplayground',
   },
@@ -87,15 +109,25 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
 })
 
+-- Fix conceallevel for json files
+
+vim.api.nvim_create_autocmd({ 'FileType' }, {
+  group = augroup('json_conceal'),
+  pattern = { 'json', 'jsonc', 'json5' },
+  callback = function()
+    vim.opt_local.conceallevel = 0
+  end,
+})
+
 -- Auto create dir when saving a file, in case some intermediate directory does not exist
 
 vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   group = augroup('auto_create_dir'),
   callback = function(event)
-    if event.match:match('^%w%w+://') then
+    if event.match:match('^%w%w+:[\\/][\\/]') then
       return
     end
-    local file = vim.loop.fs_realpath(event.match) or event.match
+    local file = vim.uv.fs_realpath(event.match) or event.match
     vim.fn.mkdir(vim.fn.fnamemodify(file, ':p:h'), 'p')
   end,
 })
